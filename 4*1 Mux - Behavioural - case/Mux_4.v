09e7ba04e32eab399e5844364b9a266464977133
K Ajaykumar <97889013+ajay-2002@users.noreply.github.com>
2024-07-05 12:04:04 +0530

    Create Mux_4.v

---
+++ 4*1 Mux - Behavioural - case/Mux_4.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+module Mux_4(
+input [3:0] i,
+input [1:0] sel,
+output reg y
+);
+always@(i or sel)
+begin
+case(sel)
+    2'b00: y = i[0];
+    2'b01: y = i[1];
+    2'b10: y = i[2];
+    2'b11: y = i[3];
+    default: y = 1'b0;
+endcase
+end
+endmodule
